8c5e3ea98aed8b1343b93a1adbfc2bd127fb65b1
ivkndm <ivakindmi@gmail.com>
2024-10-21 22:05:24 +0300

    2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,19 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b){ return a+b;
+
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+    public int dif(int a, int b){ return a-b;
+
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+    public int div(int a, int b){ return a/b;
+
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+    public int times(int a, int b){ return a*b;
+
     }
     public int solver(){
-        //TODO inser your realisation in method solver
+        int a=7;
+        int b=1;
+        return dif(a,b)-b;
     }
 }
